a9aeebadd952a365cb6e8842cafa76c2e1085edd
Matthew Fortier <matthewfortier@macbookpro.lan>
2026-06-14 21:28:36 -0400

    Update results through match nine

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const BUDGET_CAP = 150;
 const STORAGE_KEY = "calcuttaStateDraft20260609Final12";
 const SCENARIO_KEY = "calcuttaScenarioCalculatorV1";
-const RESULTS_VERSION = "2026-06-14-match-8";
+const RESULTS_VERSION = "2026-06-14-match-9";
 const ESTIMATED_DRAW_TEAM_RESULTS = 36;
 
 const players = [
@@ -260,6 +260,7 @@ const completedMatchScores = [
   { matchNumber: 5, homeGoals: 1, awayGoals: 1 },
   { matchNumber: 6, homeGoals: 0, awayGoals: 1 },
   { matchNumber: 7, homeGoals: 2, awayGoals: 0 },
+  { matchNumber: 9, homeGoals: 7, awayGoals: 1 },
 ];
 
 const scoreByMatch = Object.fromEntries(completedMatchScores.map((score) => [score.matchNumber, score]));
